9ab37c3431ab69c8e215575b3456e3054926597f
marcelo <marina19.software@gmail.com>
2026-04-27 11:58:39 -0300

    cria o command run_all

---
+++ scripts/run_all.sql
@@ -0,0 +1,8 @@
+SOURCE schema/001_create_database.sql;
+
+SOURCE schema/002_tables/001_usuarios.sql;
+SOURCE schema/002_tables/002_catalogo.sql;
+SOURCE schema/002_tables/003_estoque.sql;
+SOURCE schema/002_tables/004_pedidos.sql;
+SOURCE schema/002_tables/005_pagamentos.sql;
+SOURCE schema/002_tables/006_servicos.sql;
